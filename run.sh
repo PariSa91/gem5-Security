10a8c109ca60c34a49c08b3858822b2a7f3702c3
parisamir <parisamir@tamu.edu>
2020-06-25 13:10:32 -0500

    for gilles

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+line="module load intel/2017b;module load SCons/3.0.1-fosscuda-2018b-Python-2.7.15;module load M4/1.4.18-GCCcore-7.3.0; module load Autoconf/2.69-GCCcore-7.3.0 ;module load protobuf/3.4.0-intel-2017b ;module load git/2.14.1-GCCcore-6.4.0 ;module load gperftools/2.6.1-GCCcore-6.3.0;module load protobuf-python/3.4.0-intel-2017b-Python-2.7.14;SCons/2.5.1-GCCcore-6.3.0-Python-2.7.12-bare;module load protobuf-python/3.6.0-fosscuda-2018b-Python-3.6.6; module load intel/2017b;  module load protobuf-python/3.6.0-fosscuda-2018b-Python-3.6.6;
+eval $line
+#SCons/3.0.1-fosscuda-2018b-Python-2.7.15
